2a189ef5575e9364f7d06b4a70577e9b2ea33b2b
Nico <admin@nicofrom.space>
2025-02-09 12:39:28 +0300

    metrics

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,55 @@ export default function RootLayout({ children }: { children: any }) {
                     backgroundColor: "#151515",
                 }}
             >
+                {/* Google Metrics */}
+
+                <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TJF3PRW4');`}</script>
+
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-TJF3PRW4"
+                        height="0"
+                        width="0"
+                        style={{
+                            display: "none",
+                            visibility: "hidden",
+                        }}
+                    ></iframe>
+                </noscript>
+
+                {/* End Google Metrics */}
+
+                {/* Yandex Metrics */}
+
+                <script type="text/javascript">
+                    {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(99851128, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });`}
+                </script>
+                <noscript>
+                    <div>
+                        <img
+                            src="https://mc.yandex.ru/watch/99851128"
+                            style={{ position: "absolute", left: "-9999px" }}
+                            alt=""
+                        />
+                    </div>
+                </noscript>
+
+                {/* End Yandex Metrics */}
+
                 <MantineProvider theme={theme} forceColorScheme="dark">
                     <Notifications autoClose={2000} position="bottom-center" />
                     <ModalsProvider>
--- components/Constants.ts
+++ components/Constants.ts
@@ -1,1 +1,1 @@
-export const API_BASE = process.env.API_BASE || "http://apis.lampamc.ru/goodevening"
+export const API_BASE = process.env.API_BASE || "https://apis.lampamc.ru/goodevening"
