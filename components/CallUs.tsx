c2276abc084c4a397605f4aba654d05a03a5eca7
Nico <admin@nicofrom.space>
2025-02-09 11:35:10 +0300

    phone number duh

--- components/CallUs.tsx
+++ components/CallUs.tsx
@@ -7,7 +7,7 @@ export default function CallUs() {
                 Остались вопросы? Позвоните нам и мы поможем
             </Title>
             <Title ta="center" ff="Segoe UI Bold">
-                +7 (911) 111-11-11
+                +7 (995) 835-02-69
             </Title>
         </Stack>
     );
